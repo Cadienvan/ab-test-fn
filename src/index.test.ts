ed94cdeb8574d5804db07f8c65359a176583e5d6
Michael Di Prisco <cadienvan@gmail.com>
2022-12-21 15:42:08 +0100

    feat: first release

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,5 +1,99 @@
-import { sayHello } from '.';
+import { AB } from '.';
 
-it('should display "Hello World!"', () => {
-  expect(sayHello()).toBe('Hello World!');
+describe('Errors to be thrown', () => {
+  it('should throw an error if no arguments are provided', () => {
+    expect(() => AB()).toThrow();
+  });
+
+  it('should throw an error if only one argument is provided', () => {
+    expect(() => AB(() => {})).toThrow();
+  });
+
+  it('should throw if last argument is not an array', () => {
+    expect(() =>
+      AB(
+        () => {},
+        () => {},
+        'not an array'
+      )
+    ).toThrow();
+  });
+
+  it('should throw if the number of weights is not equal to the number of functions', () => {
+    expect(() =>
+      AB(
+        () => {},
+        () => {},
+        () => {},
+        [1, 2]
+      )
+    ).toThrow();
+  });
+
+  it('should throw if any weight is not a number', () => {
+    expect(() =>
+      AB(
+        () => {},
+        () => {},
+        [1, 'not a number']
+      )
+    ).toThrow();
+  });
+
+  it('should throw if any weight is negative', () => {
+    expect(() =>
+      AB(
+        () => {},
+        () => {},
+        [1, -1]
+      )
+    ).toThrow();
+  });
+
+  it('should throw if any argument except the last is not a function', () => {
+    expect(() =>
+      AB(
+        () => {},
+        () => {},
+        [1, 1],
+        [1, 1, 1]
+      )
+    ).toThrow();
+  });
+});
+
+describe('AB function', () => {
+  it('should return a function', () => {
+    expect(
+      typeof AB(
+        () => {},
+        () => {},
+        [1, 1]
+      )
+    ).toBe('function');
+  });
+
+  it('should return the first function if the random number is less than the first weight', () => {
+    Math.random = jest.fn().mockReturnValueOnce(0.1);
+    const fn1 = jest.fn();
+    const fn2 = jest.fn();
+    const result = AB(fn1, fn2, [1, 1]);
+    expect(result).toBe(fn1);
+  });
+
+  it('should return the second function if the random number is greater than the first weight', () => {
+    Math.random = jest.fn().mockReturnValueOnce(0.5);
+    const fn1 = jest.fn();
+    const fn2 = jest.fn();
+    const result = AB(fn1, fn2, [0.3, 0.7]);
+    expect(result).toBe(fn2);
+  });
+
+  it('should return the first function if the random number is equal to the first weight', () => {
+    Math.random = jest.fn().mockReturnValueOnce(1);
+    const fn1 = jest.fn();
+    const fn2 = jest.fn();
+    const result = AB(fn1, fn2, [1, 1]);
+    expect(result).toBe(fn2);
+  });
 });
---
+++ src/lib.ts
@@ -0,0 +1,42 @@
+export function AB(...args: unknown[]): unknown {
+  if (args.length < 3) {
+    throw new Error(
+      'You must provide at least 3 arguments. At least two functions must be provided and an array of weights'
+    );
+  }
+
+  const fns = args.slice(0, -1);
+  const weights = args[args.length - 1];
+
+  if (!Array.isArray(weights)) {
+    throw new Error('The last argument must be an array of weights');
+  }
+
+  if (args.length - 1 !== weights.length) {
+    throw new Error(
+      'The number of weights must be equal to the number of functions'
+    );
+  }
+
+  if (weights.some((weight: unknown) => typeof weight !== 'number')) {
+    throw new Error('All weights must be numbers');
+  }
+
+  if (weights.some((weight: number) => weight < 0)) {
+    throw new Error('All weights must be positive');
+  }
+
+  if (fns.some((fn: unknown) => typeof fn !== 'function')) {
+    throw new Error('All arguments except the last must be functions');
+  }
+
+  const totalWeight = weights.reduce((a, b) => a + b, 0);
+  const random = Math.random() * totalWeight;
+  let weight = 0;
+  for (let i = 0; i < fns.length; i++) {
+    weight += weights[i];
+    if (random <= weight) {
+      return fns[i];
+    }
+  }
+}
